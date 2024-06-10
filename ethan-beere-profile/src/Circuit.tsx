ee8bc6482d172193b51d1c8bb3a7ecee5fc2736f
rootbeerefloat <ethanbeere123@gmail.com>
2024-06-10 14:28:51 -0600

    Added a second canvas for the sparks

--- ethan-beere-profile/src/Circuit.tsx
+++ ethan-beere-profile/src/Circuit.tsx
@@ -4,14 +4,34 @@ import animateCircuit from './hooks/circuitCode';
 
 const CircuitCanvas = styled.canvas`
   width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;`
+
+const SparkCanvas = styled.canvas`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;`
+
+const CircuitContainer = styled.div`
+  position: relative;
+  width: 100%;
   height: 100vh;`
 
 const circuit = () => {
-    animateCircuit();
+  animateCircuit();
 
-    return (
-        <CircuitCanvas id="CircuitCanvas" />
-    );
+  return (
+    <CircuitContainer>
+      <CircuitCanvas id="CircuitCanvas" />
+      <SparkCanvas id="SparkCanvas" />
+    </CircuitContainer>
+  );
 };
 
 export default circuit;
